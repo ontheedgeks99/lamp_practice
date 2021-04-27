26f069daac9df89fc05f68aa324be7e4b69533bf
“Koya <on.the.edge-99@i.softbank.jp>
2021-04-28 08:12:41 +0900

    #15

--- www/html/assets/javascript/eventlistener.js
+++ www/html/assets/javascript/eventlistener.js
@@ -1,7 +1,4 @@
 
-var r = $('#select_box option:selected').val()
-console.log(r);
-
 window.onload = function(){
     var select = document.querySelector("#select_box");
     select.addEventListener('change', function(){
